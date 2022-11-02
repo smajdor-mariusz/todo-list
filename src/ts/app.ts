1b9a255aaace4b26da1b9ced3b2376a4f7b674eb
Mariusz <smajdortech@gmail.com>
2022-11-02 16:35:52 +0100

    feat: New list add ✨

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,19 +1,44 @@
 import { List } from './types';
 import { createListEl } from './utils/dom-utils.js';
 
-let lists: List[] = [
-  { id: '1', name: 'List 1', tasks: [] },
-  { id: '2', name: 'List 2', tasks: [] },
-];
+export let lists: List[] = [];
 
 function renderLists() {
   const listsEl = document.querySelector('[data-lists]');
 
   lists.forEach(list => {
+    if (list.rendered) return;
+    list.rendered = true;
     listsEl.appendChild(createListEl(list.name));
   });
 }
 
+function addNewList() {
+  const listInputEl: HTMLInputElement =
+    document.querySelector('[data-list-input]');
+  const newListName: string = listInputEl.value;
+
+  lists = [
+    ...lists,
+    {
+      id: Date.now().toString(),
+      name: newListName,
+      tasks: [],
+      rendered: false,
+    },
+  ];
+
+  renderLists();
+}
+
 (function init() {
+  const listFormEl: HTMLFormElement =
+    document.querySelector('[data-list-form]');
+
+  listFormEl.addEventListener('submit', e => {
+    e.preventDefault();
+    addNewList();
+  });
+
   renderLists();
 })();
--- src/ts/types.ts
+++ src/ts/types.ts
@@ -1,10 +1,12 @@
 interface Task {
   name: string;
   done: boolean;
+  rendered: boolean;
 }
 
 export interface List {
   id: string;
   name: string;
+  rendered: boolean;
   tasks: Task[];
 }
