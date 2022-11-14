8a04c76ba533e83fe4c1b083c5dba45e642dd3d9
Mariusz <smajdortech@gmail.com>
2022-11-14 16:19:11 +0100

    feat: Remove ts code ✨

--- src/ts/utils/getInputValue.ts
+++
@@ -1,7 +0,0 @@
-export function getInputValue(selector: string) {
-  const inputEl: HTMLInputElement = document.querySelector(selector);
-  const inputValue: string = inputEl.value;
-  inputEl.value = '';
-
-  return inputValue.trim();
-}
