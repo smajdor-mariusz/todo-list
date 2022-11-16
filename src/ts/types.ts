6d8f69685ae777a3d2e098b66695c4703f6cfdb9
Mariusz <smajdortech@gmail.com>
2022-11-16 16:06:56 +0100

    feat: Remove rendered property from Task and List interfaces ✨

--- src/ts/types.ts
+++ src/ts/types.ts
@@ -2,12 +2,10 @@ export interface List {
   id: string;
   name: string;
   active: boolean;
-  rendered: boolean;
   tasks: Task[];
 }
 
 export interface Task {
   name: string;
   done: boolean;
-  rendered: boolean;
 }
