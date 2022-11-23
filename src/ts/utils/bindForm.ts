6c5f1b8573ec6d538e4b9b2158e9ce83eb41376b
Mariusz <smajdortech@gmail.com>
2022-11-23 17:11:01 +0100

    refactor: Form element event listener callback parameter name change 📦

--- src/ts/utils/bindForm.ts
+++ src/ts/utils/bindForm.ts
@@ -2,8 +2,8 @@ export function bindForm(block: string, addNewItem: Function) {
   const formEl: HTMLFormElement = document.querySelector(
     `[data-${block}-form]`
   );
-  formEl.addEventListener('submit', (event: Event) => {
-    event.preventDefault();
+  formEl.addEventListener('submit', e => {
+    e.preventDefault();
     addNewItem();
   });
 }
