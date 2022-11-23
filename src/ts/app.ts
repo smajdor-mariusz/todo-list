8a5548b1e95ecad3cd4466d7c8bdddaa6851ea6b
Mariusz <smajdortech@gmail.com>
2022-11-23 17:20:17 +0100

    feat: Local storage load function ✨

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,14 +1,13 @@
 import { List } from './types';
-import { saveAndRender } from './helpers/render.helpers.js';
+import { ACTIVE_LIST_KEY, LISTS_KEY } from './constants';
 import { bindForm } from './utils/bindForm.js';
-import { ACTIVE_LIST_KEY, LISTS_KEY } from './helpers/local-storage.helper.js';
+import { saveAndRender } from './helpers/render.helpers.js';
+import { loadFromLocalStorage } from './helpers/local-storage.helpers.js';
 import { addNewList, addNewTask } from './helpers/task.helpers';
 import { bindListEvents } from './helpers/bind-event.helpers';
 
-export let lists: List[] = JSON.parse(localStorage.getItem(LISTS_KEY)) || [];
-export let activeListId: string = JSON.parse(
-  localStorage.getItem(ACTIVE_LIST_KEY)
-);
+export let lists: List[] = loadFromLocalStorage(LISTS_KEY) || [];
+export let activeListId: string = loadFromLocalStorage(ACTIVE_LIST_KEY);
 
 export const setActiveListId = (value: string) => (activeListId = value);
 
--- src/ts/helpers/local-storage.helpers.ts
+++ src/ts/helpers/local-storage.helpers.ts
@@ -1,9 +1,11 @@
 import { lists, activeListId } from '../app';
-
-export const LISTS_KEY = 'todo.lists';
-export const ACTIVE_LIST_KEY = 'todo.activeList';
+import { ACTIVE_LIST_KEY, LISTS_KEY } from '../constants';
 
 export function saveInLocalStorage() {
   localStorage.setItem(LISTS_KEY, JSON.stringify(lists));
   localStorage.setItem(ACTIVE_LIST_KEY, JSON.stringify(activeListId));
+}
+
+export function loadFromLocalStorage(key: string) {
+  return JSON.parse(localStorage.getItem(key));
 }
