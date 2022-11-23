aafde306f346b2026634032602bba4001ac42aaf
Mariusz <smajdortech@gmail.com>
2022-11-23 16:10:07 +0100

    refactor: Task helper functions grouped into folder 📦

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,64 +1,24 @@
 import { List } from './types';
 import { saveAndRender } from './helpers/render.helpers.js';
-import { createNewItem } from './utils/createNewItem.js';
 import { bindForm } from './utils/bindForm.js';
 import { ACTIVE_LIST_KEY, LISTS_KEY } from './helpers/local-storage.helper.js';
-import { addNewList } from './helpers/task.helpers';
+import {
+  addNewList,
+  addNewTask,
+  clearCompletedTasks,
+  deleteList,
+  deleteTask,
+  toggleTaskDone,
+} from './helpers/task.helpers';
 
 let lists: List[] = JSON.parse(localStorage.getItem(LISTS_KEY)) || [];
 let activeListId: string = JSON.parse(localStorage.getItem(ACTIVE_LIST_KEY));
-
-function addNewTask() {
-  const newTaskName: string = createNewItem('[data-task-input]');
-  if (!newTaskName) return;
-  const activeList = lists.find(list => list.id === activeListId);
-  activeList.tasks.push({
-    name: newTaskName,
-    done: false,
-  });
-  saveAndRender(lists, activeListId, bindTaskEvents);
-}
-
-function toggleTaskDone(index: string) {
-  const activeList = lists.find(list => list.id === activeListId);
-  activeList.tasks[index].done = !activeList.tasks[index].done;
-  saveAndRender(lists, activeListId, bindTaskEvents);
-}
-
-function deleteTask(index: number) {
-  const activeList = lists.find(list => list.id === activeListId);
-  activeList.tasks.splice(index, 1);
-  saveAndRender(lists, activeListId, bindTaskEvents);
-}
-
-function clearCompletedTasks() {
-  const clearCompletedButtonEl: HTMLButtonElement = document.querySelector(
-    '[data-clear-completed]'
-  );
-  clearCompletedButtonEl.addEventListener('click', () => {
-    const activeList = lists.find(list => list.id === activeListId);
-    lists.find(list => list.id === activeListId).tasks =
-      activeList.tasks.filter(task => task.done === false);
-
-    saveAndRender(lists, activeListId, bindTaskEvents);
-  });
-}
-
-function deleteList() {
-  const deleteListBtnEl: HTMLButtonElement =
-    document.querySelector('[data-delete-list]');
-  deleteListBtnEl.addEventListener('click', () => {
-    lists = lists.filter(list => list.id !== activeListId);
-    activeListId = null;
-    saveAndRender(lists, activeListId, bindTaskEvents);
-  });
-}
 
 function bindToggleButtons() {
   const toggleButtonEls = document.querySelectorAll('[data-toggle]');
   toggleButtonEls.forEach((button: HTMLButtonElement, index: number) => {
     button.addEventListener('click', () => {
-      toggleTaskDone(index.toString());
+      toggleTaskDone(index.toString(), lists, activeListId, bindTaskEvents);
     });
   });
 }
@@ -67,7 +27,7 @@ function bindDeleteButtons() {
   const deleteButtonsEls = document.querySelectorAll('[data-delete]');
   deleteButtonsEls.forEach((button: HTMLButtonElement, index: number) => {
     button.addEventListener('click', () => {
-      deleteTask(index);
+      deleteTask(index, lists, activeListId, bindTaskEvents);
     });
   });
 }
@@ -78,8 +38,8 @@ function bindTaskEvents() {
 }
 
 function bindListEvents() {
-  clearCompletedTasks();
-  deleteList();
+  clearCompletedTasks(lists, activeListId, bindTaskEvents);
+  deleteList(activeListId, lists, bindTaskEvents);
 }
 
 function init() {
--- src/ts/helpers/task.helpers.ts
+++ src/ts/helpers/task.helpers.ts
@@ -2,6 +2,43 @@ import { List } from '../types';
 import { createNewItem } from '../utils/createNewItem';
 import { saveAndRender } from './render.helpers';
 
+export function addNewTask(
+  lists: List[],
+  activeListId: string,
+  bindTaskEvents: Function
+) {
+  const newTaskName: string = createNewItem('[data-task-input]');
+  if (!newTaskName) return;
+  const activeList = lists.find(list => list.id === activeListId);
+  activeList.tasks.push({
+    name: newTaskName,
+    done: false,
+  });
+  saveAndRender(lists, activeListId, bindTaskEvents);
+}
+
+export function toggleTaskDone(
+  index: string,
+  lists: List[],
+  activeListId: string,
+  bindTaskEvents: Function
+) {
+  const activeList = lists.find(list => list.id === activeListId);
+  activeList.tasks[index].done = !activeList.tasks[index].done;
+  saveAndRender(lists, activeListId, bindTaskEvents);
+}
+
+export function deleteTask(
+  index: number,
+  lists: List[],
+  activeListId: string,
+  bindTaskEvents: Function
+) {
+  const activeList = lists.find(list => list.id === activeListId);
+  activeList.tasks.splice(index, 1);
+  saveAndRender(lists, activeListId, bindTaskEvents);
+}
+
 export function addNewList(
   lists: List[],
   activeListId: string,
@@ -15,4 +52,35 @@ export function addNewList(
     tasks: [],
   });
   saveAndRender(lists, activeListId, bindTaskEvents);
+}
+
+export function deleteList(
+  activeListId: string,
+  lists: List[],
+  bindTaskEvents: Function
+) {
+  const deleteListBtnEl: HTMLButtonElement =
+    document.querySelector('[data-delete-list]');
+  deleteListBtnEl.addEventListener('click', () => {
+    lists = lists.filter(list => list.id !== activeListId);
+    activeListId = null;
+    saveAndRender(lists, activeListId, bindTaskEvents);
+  });
+}
+
+export function clearCompletedTasks(
+  lists: List[],
+  activeListId: string,
+  bindTaskEvents: Function
+) {
+  const clearCompletedButtonEl: HTMLButtonElement = document.querySelector(
+    '[data-clear-completed]'
+  );
+  clearCompletedButtonEl.addEventListener('click', () => {
+    const activeList = lists.find(list => list.id === activeListId);
+    lists.find(list => list.id === activeListId).tasks =
+      activeList.tasks.filter(task => task.done === false);
+
+    saveAndRender(lists, activeListId, bindTaskEvents);
+  });
 }
